e7ab096e899e42959a6cea6ce3d4b71bfc17cbc2
DHARUN <159411407+DHARUN437@users.noreply.github.com>
2024-10-03 19:14:40 +0530

    Dharun Data Scientist portfolio

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="DHARUN Portfolio",
                    layout="wide",
 )
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
-resume_file = current_dir / 'assets' /"Dharun_ds.pdf"
+resume_file = current_dir / "assets\Dharun_Datascientist.pdf"
 css_file = current_dir / 'styles\homepage.css'
 
 with open(css_file) as f:
@@ -21,7 +21,7 @@ with open(resume_file, "rb") as pdf_file:
     
 st.markdown("<marquee behavior='scroll' direction='left' style='font-size:30px;'>Welcome to my portfolio</marquee><br><br><br>", unsafe_allow_html=True)
 
-img = Image.open("assets\WhatsApp Image 2024-09-10 at 2.32.55 PM.jpeg")
+img = Image.open("assets\WhatsApp Image 2024-10-03 at 4.50.36 PM-modified.png")
 
 with st.container():
     left_column, middle_column, right_column = st.columns((1,0.2,0.6))
@@ -46,26 +46,40 @@ with st.container():
 
 st.subheader("Education", divider="red")
 
-st.write("###  [_Sri Ramakrishna College Of Arts and Science_](https://www.srcas.ac.in/) (2021-2024)")
-st.write("### Bacholer of Science")
-st.write("##### Major: Information Technology")
-st.write("**Relevant Coursework:** Data Science,Machine learning, Deep Learning, Natural Language Processing....")
+srcas = Image.open("assets\download-modified.png")
+
+image_column, text_column = st.columns((1,5))
+with image_column:
+        st.image(srcas)
+with text_column:
+ st.write("###  [_Sri Ramakrishna College Of Arts and Science_](https://www.srcas.ac.in/) (2021-2024)")
+ st.write("### Bacholer of Science")
+ st.write("##### Major: Information Technology")
+ st.write("**Relevant Coursework:** Data Science,Machine learning, Deep Learning, Natural Language Processing....")
 st.markdown("<br>", unsafe_allow_html=True)
 
 st.subheader("Experience", divider="red")
-st.write("### Data Scientist Intern - Qtree technologies, Coimbatore")
-st.write("##### Key Responsiblities:")
-st.markdown("""
-            -> Gained hands-on experience in data manipulation, data analysis, and statistical modeling using Python and SQL.<br>
-            -> Developed machine learning models, including regression, classification, and clustering techniques.
-<br>-> Performed data preprocessing tasks such as data cleaning, feature engineering, and handling missing values. Built and deployed interactive dashboards for data visualization using **Seaborn** and __Matplotlib__.<br>
+
+qtree = Image.open("assets\qtree-modified.png")
+
+with st.container():
+    image_column, text_column = st.columns((1,5))
+    with image_column:
+        st.image(qtree)
+    with text_column:
+       st.write("### Data Scientist Intern - Qtree technologies, Coimbatore")
+       st.write("##### Key Responsiblities:")
+       st.markdown("""
+             ► Gained hands-on experience in data manipulation, data analysis, and statistical modeling using Python and SQL.<br>
+             ► Developed machine learning models, including regression, classification, and clustering techniques.<br>
+             ► Performed data preprocessing tasks such as data cleaning, feature engineering, and handling missing values. Built and deployed interactive dashboards for data visualization using **Seaborn** and __Matplotlib__.<br>
 """, unsafe_allow_html=True)
-st.write("##### Key Achievements:")
-st.markdown("""
-            -> Created a predictive model for Cyberbullying detection, achieving an accuracy of __90%__ using [ML techniques like LinearSVC, TF-IDF vectorizer].<br>
-            -> Developed a data pipeline to automate the collection, cleaning, and analysis of data, reducing processing time by
-__65%__.
-<br>-> Presented a final capstone project on Cyberbullying, demonstrating proficiency in machine learning algorithms and data storytelling.
+       st.write("##### Key Achievements:")
+       st.markdown("""
+         ► Created a predictive model for Cyberbullying detection, achieving an accuracy of __90%__ using [ML techniques like LinearSVC, TF-IDF vectorizer].<br>
+         ► Developed a data pipeline to automate the collection, cleaning, and analysis of data, reducing processing time by
+__65%__.<br>
+         ► Presented a final capstone project on Cyberbullying, demonstrating proficiency in machine learning algorithms and data storytelling.
 """, unsafe_allow_html=True)
 
 st.markdown("<br>", unsafe_allow_html=True)
@@ -160,10 +174,10 @@ with st.container():
                       
                        Python, Streamlit, Scikit-learn, Natural Language Processing (NLP), TF-IDF Vectorizer, LinearSVC, Joblib
 __Description__ : <br>
- -> Developed a machine learning-based web application to detect cyberbullying in text data.<br>
- -> Implemented data preprocessing techniques, including stopword removal and text vectorization using TF-IDF.<br>
- -> Built an interactive Streamlit web app to provide real-time feedback on user-submitted text.<br>
- -> The system uses a pre-trained LinearSVC model and TF-IDF Vectorizer to classify input text as either "bullying" or "not bullying"<br>
+ - ► Developed a machine learning-based web application to detect cyberbullying in text data.<br>
+ - ► Implemented data preprocessing techniques, including stopword removal and text vectorization using TF-IDF.<br>
+ - ► Built an interactive Streamlit web app to provide real-time feedback on user-submitted text.<br>
+ - ► The system uses a pre-trained LinearSVC model and TF-IDF Vectorizer to classify input text as either "bullying" or "not bullying"<br>
             """, unsafe_allow_html=True)       
         mention(label="Streamlit App", icon="streamlit", url="https://cyberbullyingdetection-dharun.streamlit.app/")
         mention(label="Github Repo", icon="github", url="https://github.com/DHARUN437/cyberbullying_detection")
@@ -182,10 +196,10 @@ with st.container():
                       
                        Python, Streamlit, Logistic regression, Scikit-learn, Pandas, Numpy, Joblib
 __Description__ : <br>
- ->  Developed an interactive web application using Streamlit to predict the nature of breast tumors based on patient data.<br>
- -> The app utilizes a trained Logistic Regression model to classify tumors as either malignant or benign, providing critical insights for early detection<br>
- -> Designed and implemented a user-friendly interface to allow input of patient ID and display relevant medical parameters.<br>
- -> Integrated a trained machine learning model using joblib, enabling real-time predictions based on user-provided data.<br>
+ - ►  Developed an interactive web application using Streamlit to predict the nature of breast tumors based on patient data.<br>
+ - ► The app utilizes a trained Logistic Regression model to classify tumors as either malignant or benign, providing critical insights for early detection<br>
+ - ► Designed and implemented a user-friendly interface to allow input of patient ID and display relevant medical parameters.<br>
+ - ► Integrated a trained machine learning model using joblib, enabling real-time predictions based on user-provided data.<br>
             """ , unsafe_allow_html=True)       
         mention(label="Streamlit App", icon="streamlit", url="https://breastcancer-classification-dharun.streamlit.app/")
         mention(label="Github Repo", icon="github", url="https://github.com/DHARUN437/breast_cancer-classification")
@@ -205,10 +219,10 @@ with st.container():
                       
                        Python, Streamlit, Scikit-learn, Natural Language Processing (NLP), Textblob, Vender, XGBoost, Joblib
 __Description__ : <br>
- -> This project focuses on building a Natural Language Processing (NLP) model to classify customer reviews of restaurants into positive or negative sentiment.<br>
- -> Preprocessing of text data using tokenization, removal of stopwords, stemming, and lemmatization.<br>
- -> Real-time sentiment analysis using both TextBlob and VADER models.<br>
- -> The application is deployed using Streamlit, allowing users to interact with the model in real-time.<br>
+ - ► This project focuses on building a Natural Language Processing (NLP) model to classify customer reviews of restaurants into positive or negative sentiment.<br>
+ - ► Preprocessing of text data using tokenization, removal of stopwords, stemming, and lemmatization.<br>
+ - ► Real-time sentiment analysis using both TextBlob and VADER models.<br>
+ - ► The application is deployed using Streamlit, allowing users to interact with the model in real-time.<br>
             """ , unsafe_allow_html=True)       
         mention(label="Streamlit App", icon="streamlit", url="https://restaurant-sentiment-dharun.streamlit.app/")
         mention(label="Github Repo", icon="github", url="https://github.com/DHARUN437/NLP_projects")
@@ -217,7 +231,7 @@ __Description__ : <br>
         st.image(res)
         st.markdown("<br>",unsafe_allow_html=True)
         
-st.subheader("Minor Projects", divider= 'red')
+st.subheader("Other Projects", divider= 'red')
 
 col1, col2, col3 = st.columns(3)
 
@@ -225,19 +239,64 @@ with col1:
     st.image(r"assets\salary.png", caption="Software developer salary prediction", use_column_width=True)
 
 with col2:
-    st.image(r"assets\cyber_bully.png", caption="Description for Image 2", use_column_width=True)
+    st.image(r"assets\heart.png", caption="Heart Disease prediction", use_column_width=True)
 
 with col3:
-    st.image(r"assets\res_review.png", caption="Description for Image 3", use_column_width=True)
+    st.image(r"assets\gold_price.png", caption="Gold price prediction", use_column_width=True)
     
 
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.image(r"assets\brest_cancer.png", caption="Description for Image 1", use_column_width=True)
+    st.image(r"assets\wine.png", caption="Wine quality prediction", use_column_width=True)
 
 with col2:
-    st.image(r"assets\cyber_bully.png", caption="Description for Image 2", use_column_width=True)
+    st.image(r"assets\car_price.png", caption="Car price prediction", use_column_width=True)
 
 with col3:
-    st.image(r"assets\res_review.png", caption="Description for Image 3", use_column_width=True)
+    st.image(r"assets\diabetics.png", caption="Diabetics prediction", use_column_width=True)
+    
+    
+st.subheader("Certifications", divider="red")
+st.markdown("<br>", unsafe_allow_html=True)
+st.subheader("[Python for Data Science - IBM Developer Skills](https://courses.cognitiveclass.ai/certificates/9f531c4035844be6b5050a80bf518844)", divider='blue')
+st.markdown("""
+            - ► Completed an introductory course designed to build Python programming skills from the ground up, specifically tailored for data science applications.
+            - ► Acquired fundamental Python programming skills, including variables, expressions, string operations, and data structures (lists, tuples, sets, and dictionaries).
+            - ► Mastered core programming concepts such as conditions, loops, functions, and object-oriented programming.
+            - ► Gained hands-on experience with data manipulation using Pandas, including reading, writing, and processing data.
+            - ► Developed proficiency in handling NumPy arrays for numerical computations and working with simple APIs.
+            """, )
+st.markdown("<br>", unsafe_allow_html=True)
+st.subheader("[British Airways Data Science Job Simulation - Forage](https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/British%20Airways/NjynCWzGSaWXQCxSX_British%20Airways_jMEpwCThPTfPEtWe8_1723800889835_completion_certificate.pdf)", divider='blue')
+st.markdown("""
+            - ► In this job simulation, I have learned Data Manipulation, Data Science, Data Visualisation, Machine Learning, PowerPoint, Python, Web Scraping 
+            - ► Completed a simulation focussing on how data science is a critical component of British Airways success 
+            variance, cross-validation, and improving learning algorithms' efficiency.
+            - ►Scraped and analysed customer review data to uncover findings
+            - ► Built a predictive model to understand factors that influence buying behaviour
+            """, )
+st.markdown("<br>", unsafe_allow_html=True)
+st.subheader("[Python Basics - Hackerrank](https://www.hackerrank.com/certificates/iframe/bda957b41f34)", divider="blue")
+st.write('''
+                 - ► I learned the basics of python programming which helped me to move one step closer to the Data Science and Machine Learning.
+                 - ► It covers topics like Scalar Types, Operators and Control Flow, Strings, Collections and Iteration, Modularity, Objects and Types and Classes 
+                                  ''')
+st.subheader("[Pandas Introduction - Simplilearn](https://simpli-web.app.link/e/A3uyMoanoNb)", divider="blue")
+st.write('''
+                 - ► Gained hands-on experience with Pandas, a popular Python library for data manipulation and analysis. 
+                 - ► Learned to perform essential data operations like : 
+                              Loading and manipulating datasets,
+                              Filtering, sorting, and summarizing data,
+                              Handling missing data and merging datasets.
+                 - ► Developed efficient workflows for cleaning and preparing data for analysis using Pandas' powerful data structures, such as DataFrames and Series.
+                 ''')
+st.markdown("<br>", unsafe_allow_html=True)
+st.subheader("Contact",divider='red')
+
+st.write("[📧 Email : bdharun1912@gmail.com](bdharun1912@gmail.com)")
+st.write("📞 Phone : 9791817437")
+st.markdown(social_icons(32, 32, LinkedIn="https://www.linkedin.com/in/bdharun19/", 
+                                         GitHub="https://github.com/DHARUN437", 
+                                         ),
+                                         unsafe_allow_html=True)
